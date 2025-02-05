0b9b24505774280ec25813fa831eae2ebab721b0
Rasmus <rasmusjensen.dev@gmail.com>
2025-02-05 21:05:43 +0100

    Made it send a sms on a scheduale

--- CuteBunnyWebApp/routes/console.php
+++ CuteBunnyWebApp/routes/console.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Logic\QuoteLogic;
+use App\Logic\SmsLogic;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+// Make it so that it will send a sms notification every day at 8:15 AM Danish time
+Schedule::call(function () {
+    $quoteLogic = new QuoteLogic();
+    $smsLogic = new SmsLogic($quoteLogic);
+    $smsLogic->SendSmsReminder();
+})->timezone('Europe/Copenhagen')->dailyAt('08:15');
+
